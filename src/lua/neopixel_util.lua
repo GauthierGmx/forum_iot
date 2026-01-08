c527ecf8c5271d52d50b48608696f71108a831a9
Gauthier GOUMEAUX <goumeaux.e2501574@etud.univ-ubs.fr>
2026-01-08 20:13:45 +0100

    Creation d'un fichier pour Neopixel

---
+++ src/lua/neopixel_util.lua
@@ -0,0 +1,16 @@
+-- Initiate Neopixel
+neo = neopixel.attach(neopixel.WS2812B,pio.GPIO23,8)
+
+function clearNeoPixel()
+   for i=0,7 do
+      neo:setPixel(i,0,0,0)
+   end
+   neo:update() 
+end
+
+function placedAnimation()
+    for i=0,7 do
+        neo:setPixel(i,255,0,0)
+    end
+    neo:update()
+end
