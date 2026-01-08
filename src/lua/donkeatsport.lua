3084bc04d496aed9bf4ce77966c688904945dd0a
Gauthier GOUMEAUX <goumeaux.e2501574@etud.univ-ubs.fr>
2026-01-08 20:14:18 +0100

    Ajout de la fonction de callback pour la fonction subscribe

--- src/lua/donkeatsport.lua
+++ src/lua/donkeatsport.lua
@@ -1,7 +1,7 @@
--- Initiate Neopixel
-neo = neopixel.attach(neopixel.WS2812B,pio.GPIO23,8)
+dofile("neopixel_util.lua")
+
 -- Initiate buzzer
-pio.pin.setdir(pio.OUTPUT,pio.GPIO22)
+pio.pin.setdir(pio.OUTPUT,pio.GPIO2)
 -- Initiate potentiometer
 -- Only GPIO22 used (button), GPIO21 and GPIO4 not used
 enc = encoder.attach(pio.GPIO4,pio.GPIO21,pio.GPIO22)
@@ -12,9 +12,35 @@ device_id = "1"
 
 -- Initiate MQTT
 id_mqtt = "donk_" .. device_id
+topic = "/donkeatsport/"..device_id
 client = mqtt.client(id_mqtt,"test.mosquitto.org",1883,false)
 
 -- Connection to the MQTT broker
 client:connect("","")
 -- Example for subscribing to a topic
--- client:subscribe("/donkeatsport/<device_id>")
+-- client:subscribe(topic,mqtt.QOS0,receivingMessage)
+
+function receivingMessage(length,message)
+        start,stop = message:find(":")
+        state = message:sub(stop+1)
+        
+        -- Order placed but not trigerred yet
+        if state == "placed" then
+                -- FIXED RED LED
+                
+        -- Order trigerred
+        elseif state == "cooking" then
+                -- ORANGE LED DOING LOADING ANIMATION
+                
+        -- Order ready
+        elseif state == "done" then
+                -- GREEN LED FLASHING
+        
+        -- Reset of the buzzer
+        elseif state == "reset" then
+                -- LED OFF
+                
+        end
+end
+
+client:subscribe(topic,mqtt.QOS0,receivingMessage)
